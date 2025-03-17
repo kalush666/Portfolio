3ec40b48aab43d695b09f2f6fee93d97f20b6d9f
kalush666 <jonathan.kalush@gmail.com>
2025-03-17 22:01:46 +0200

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import "./index.css";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {""}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+    </>
   );
 }
 
